a11dadce47e777e2c46b0497500cc51234b770ab
miora.rr <miora.r.rakoto@gmail.com>
2024-01-05 09:35:09 -0500

    Add description to TimeConversion

--- src/TimeConversion.java
+++ src/TimeConversion.java
@@ -1,4 +1,9 @@
 import java.text.*;
+/*
+* Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+    Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+          - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+*/
 public class TimeConversion {
     public static String timeConversion(String s) {
         try {
